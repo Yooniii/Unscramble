e1b8bd5428979bf1c8815970cf0af359f64ae493
Sarah Yoon <sarahyoon705@gmail.com>
2024-01-16 10:52:19 -0500

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -4,11 +4,11 @@ const playBtn = document.getElementById("start-btn");
 const playAgainBtn = document.getElementById("play-again-btn");
 const countdownEl = document.getElementById("timer");
 const startingMinutes = 1;
-var input = document.getElementById("user-input");
-var allowedKeys = [];
-var wordHistory = [];
-var scoreCount = 0;
-var word;
+const input = document.getElementById("user-input");
+let allowedKeys = [];
+let wordHistory = [];
+let scoreCount = 0;
+let word;
 let timer;
 let time;
 playBtn.addEventListener("click", startGame);
@@ -22,8 +22,8 @@ function startGame() {
 
   input.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-      if (checkLetters(input.value, allowedKeys)) {
-        checkWord(input.value); // check for previous entries + valid eng word
+      if (checkLetters(input.value.toLowerCase(), allowedKeys)) {
+        checkWord(input.value); // check for previous entries + valid  word
         input.value = "";
       }
       else {
@@ -53,20 +53,20 @@ function updateCountdown() {
 function initBoard() {
   fetch(url)
     .then(response => response.json())
-    .then(words => {
-      word = words[0];
-      wordRef.innerHTML = word;
-      wordRef.style.fontSize = "40px";
-      // initialize grey keyboard
-      for (const elem of document.getElementsByClassName("keyboard-button")) {
-        elem.style.backgroundColor = "rgb(218, 218, 218)"
-      }
-      // shade allowed letter keys 
-      for (let i=0; i<10; i++) {
-        shadeAllowedKeys(word[i]);
-        allowedKeys.push(word[i]);
-      }
-    })
+      .then(words => {
+        word = words[0];
+        wordRef.innerHTML = word;
+        wordRef.style.fontSize = "40px";
+
+        for (const elem of document.getElementsByClassName("keyboard-button")) {
+          elem.style.backgroundColor = "rgb(218, 218, 218)"
+        }
+
+        for (let i=0; i<10; i++) {
+          shadeAllowedKeys(word[i]);
+          allowedKeys.push(word[i]);
+        }
+      })
   .catch(error => console.error(error));
 }
 
@@ -92,21 +92,23 @@ function checkLetters (input, allowedKeys) {
 }
 
 async function checkWord(userWord) {
-  const response = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + userWord);
-  const data = await response.json();
-  
-    if (data.title === 'No Definitions Found') {
-      document.getElementById("error-msg").innerHTML = "Not a valid word!";
+  fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + userWord)
+    .then(response => response.json())
+      .then (data => {
+        if (data.title === 'No Definitions Found') {
+          document.getElementById("error-msg").innerHTML = "Not a valid word";
 
-    } else if (wordHistory.indexOf(userWord) === -1) {      
-      updateScore(getPoints(userWord.length));
-      document.getElementById("error-msg").innerHTML = "";
-      wordHistory.push(userWord);
+        } else if (wordHistory.indexOf(userWord) === -1) {
+          wordHistory.push(userWord);
+          updateScore(getPoints(userWord.length));
+          document.getElementById("error-msg").innerHTML ="";
 
-    } else if (wordHistory.indexOf(userWord) >= 0) {
-      document.getElementById("error-msg").innerHTML = userWord + " was already entered";
-    }
+        } else if (wordHistory.indexOf(userWord >= 0)) {
+          document.getElementById("error-msg").innerHTML = userWord + " was already entered";
+        }
+      })
 }
+
   
 function getPoints(wordLength) {
   if (wordLength >= 1 && wordLength <= 3) {
@@ -114,28 +116,28 @@ function getPoints(wordLength) {
   } else if (wordLength === 4 || wordLength=== 5) {
     return scoreCount += 4;
   } else if (wordLength >= 6 && wordLength <= 9) {
-    return scoreCount += 5;
+    return scoreCount += 6;
   } else if (wordLength >= 10) {
     return scoreCount += 10;
   }
 }
 
-function updateScore(points) {;
+function updateScore(points) {
   document.getElementById("score-window").innerHTML = "Current Score: " + points;
 }
 
 function endGame () {
   input.value = "";
   input.disabled = true;
-  const wordRef = document.getElementById("word-ref");
-  wordRef.innerHTML = "GAME OVER";
+  document.getElementById("word-ref").innerHTML = "GAME OVER";
   document.getElementById("error-msg").innerHTML = "";
   playAgainBtn.disabled = false;
 
   playAgainBtn.onclick = function () {
     if (timer) {
-    clearInterval(timer);
+      clearInterval(timer);
     }
+    
     allowedKeys = [];
     wordHistory = [];
     scoreCount = 0;
